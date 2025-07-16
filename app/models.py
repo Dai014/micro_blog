26dd3b08a9d4dc00befd6bab0d07c26557a019e3
Đại Nguyễn <dainguyen@ais-MacBook-Pro-2.local>
2025-07-16 23:55:28 +0900

    m migrate posts table

--- app/models.py
+++ app/models.py
@@ -1,29 +1,31 @@
 from datetime import datetime, timezone
-
-from app import db
-import sqlalchemy.orm as so
-import sqlalchemy as sa
 from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True)
-
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True)
-
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
-
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(500))
-    timestamp: so.Mapped[sa.DateTime] = so.mapped_column(index=True ,default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
     author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self):
